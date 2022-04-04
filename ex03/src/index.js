55ff95ee027691242b91de959a7f53de0a8946a9
AnelB <anel.b@live.com>
2022-04-04 16:37:32 +0200

    update

---
+++ ex03/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function localScope() {
+	var myVariable = "I am Here!";
+	console.log(myVariable);
+}
+// Only change code above this line
+// My Variable is not defined outside of localScope
+localScope();
+
+if(typeof myVariable != "undefined") {
+	console.log('outside localScope', myVariable) }
+else {console.log('outside localScope UNDEFINED!!!')
+	 };
+module.exports = localScope;
