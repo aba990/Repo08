b817be4e1071f4662d9cc30c111b724aaa1b5439
AnelB <anel.b@live.com>
2022-04-04 17:07:45 +0200

    up1011

---
+++ ex10/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function convertStrToInt(str) {
+	return parseInt(str);
+}
+console.log(parseInt(convertStrToInt("44")));
+console.log(parseInt(convertStrToInt("44")));
+console.log(parseInt(convertStrToInt("27")));
+console.log(parseInt(convertStrToInt("Jeff Bezos")));
+// Only change code above this line
+module.exports = convertStrToInt;
---
+++ ex11/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function convertToInt(str) {
+	return parseInt(str, 16);
+}
+console.log(parseInt(convertToInt("BA")));
+console.log(parseInt(convertToInt("BA")));
+console.log(parseInt(convertToInt("F1")));
+console.log(parseInt(convertToInt("Jeff Bezos")));
+// Only change code above this line
+module.exports = convertToInt;
