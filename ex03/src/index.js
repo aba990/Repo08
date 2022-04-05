994db97f7793eeefe2e0190d89df823679b9d6af
AnelB <anel.b@live.com>
2022-04-05 07:01:20 +0200

    update 3

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -10,5 +10,5 @@ localScope();
 if(typeof myVariable != "undefined") {
 	console.log('outside localScope', myVariable) }
 else {console.log('outside localScope UNDEFINED!!!')
-	 };
+	 }
 module.exports = localScope;
